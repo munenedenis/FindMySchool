bb3bf9e470f5ba2aebfa6560eec1df1475800f03
munenedenis <dmunene06@yahoo.com>
2012-09-24 17:17:56 +0300

    removed unused query from dttbtest.php

--- dttblstest.php
+++ dttblstest.php
@@ -124,28 +124,6 @@ echo "<tr style='text-align:left;'>
       </table>
 
 
-
-
-<?php
-
-$qweri=mysql_query("SELECT County FROM `kcpe2010` WHERE KNEC_Code IN (SELECT SUBSTRING(IndexNumber, 1, 6) FROM f1selection); ");
-?>
-<select name="County" size=9>
-
-<?php
-while($list=mysql_fetch_array($qweri))
-
-
-{
-echo 
-"<option value=" .$list['County'] . ">" . $list['County'] . "</option>";
-}
-
-echo "</select> "
-?>
-
-
-
 <footer style="float;right;">
               
   <a href="http://www.code4kenya.org">&copy; code4kenya 2012</a>
