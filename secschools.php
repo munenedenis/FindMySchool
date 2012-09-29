a2f551b6335b7911359dbcb348834a03e0eb8c16
munenedenis <dmunene06@yahoo.com>
2012-09-30 01:55:02 +0300

    change back to working version of secschools.php

--- secschools.php
+++ secschools.php
@@ -13,19 +13,26 @@
  require_once ('config.php');
  
  $sql = mysql_query("SELECT * FROM kcseresults WHERE SchoolName ='$schools'")or Die(mysql_error());
+ 
+ $total = mysql_num_rows($sql);
+ $mean = "0";
+
+
  while($row = mysql_fetch_array($sql))
  {
+  $MeanGrade = $mean + $row['MeanGrade'];
  echo "<div style='width:250px; float:left;''>
       <div class='yearbase'>".$row['Year']."</div><br />
       <div>".$row['DistrictName']."</div><br />
       <div>".$row['County']."</div><br />
       <div>".$row['Gender']."</div><br />
       <div>".$row['GradeAttained']."</div><br />
-      <div>".$row['MeanGrade']."</div><br />
+      <div>".$MeanGrade."</div><br />
       <div>".$row['Frequency']."</div><br />
 
       </div>";
 }
+$MeanGrade = $mean/$total;
 ?>
 
 
