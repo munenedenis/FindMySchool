013f5373bac8468be1fd47aee81661abb52f045e
munenedenis <dmunene06@yahoo.com>
2012-10-01 14:24:42 +0300

    results sorted by year

--- secschools.php
+++ secschools.php
@@ -5,34 +5,39 @@
  <br/><p> This are the details for <?php echo $_GET['SchoolName']; $schools=$_GET['SchoolName'];?> </p>
 
 
-
+<table><tr><th>Gender</th><th>GradeAttained</th><th>MeanGrade</th><th>Frequency</th></tr>
 
 <?php
 
  $query = mysql_real_escape_string($schools);
  require_once ('config.php');
  
- $sql = mysql_query("SELECT * FROM kcseresults WHERE SchoolName ='$schools'")or Die(mysql_error());
+ $sql = mysql_query("SELECT * FROM kcseresults WHERE Year = '2006' AND SchoolName ='$schools'")or Die(mysql_error());
  
- $total = mysql_num_rows($sql);
- $mean = "0";
+// $total = mysql_num_rows($sql);
 
+// $mean = "0";
 
  while($row = mysql_fetch_array($sql))
  {
-  $MeanGrade = $mean + $row['MeanGrade'];
- echo "<div style='width:250px; float:left;''>
-      <div class='yearbase'>".$row['Year']."</div><br />
-      <div>".$row['DistrictName']."</div><br />
-      <div>".$row['County']."</div><br />
-      <div>".$row['Gender']."</div><br />
+//  $MeanGrade = $mean + $row['MeanGrade'];
+
+echo "<tr><td>".$row['GradeAttained']."</td><td>".$row['MeanGrade']."</td><td>".$row['Frequency']."</td></tr>
+     ";
+
+/* echo "<div style='width:250px; float:left;''>
+      <div class='yearbase'></div><br />
+      <div></div><br />
+      <div></div><br />
       <div>".$row['GradeAttained']."</div><br />
       <div>".$MeanGrade."</div><br />
-      <div>".$row['Frequency']."</div><br />
+      <div></div><br />
 
-      </div>";
+      </div>"; 
+
+*/
 }
-$MeanGrade = $mean/$total;
+//$MeanGrade = $mean/$total;
 ?>
 
 
